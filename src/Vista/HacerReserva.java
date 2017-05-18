d4fdb9d8aa4924ecf7d1a9e0775c4121d02e018c
roto3 <cludefanslynxoficial@gmail.com>
2017-05-18 18:16:45 +0200

    HacerReservaPUTAZORRA

--- src/Vista/HacerReserva.java
+++ src/Vista/HacerReserva.java
@@ -13,6 +13,8 @@ import java.awt.Insets;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
 import java.awt.Font;
+import javax.swing.JList;
+import javax.swing.SwingConstants;
 
 public class HacerReserva extends JFrame {
 
@@ -44,25 +46,17 @@ public class HacerReserva extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 109, 0, 68, 0, 82, 0, 0};
+		gbl_contentPane.columnWidths = new int[]{29, 194, 87, 202, 26, 0};
 		gbl_contentPane.rowHeights = new int[]{0, 30, 0, 0, 31, 0, 0, 0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
-		
-		JLabel lblAerolnea = new JLabel("Aerol\u00EDnea");
-		lblAerolnea.setFont(new Font("Rockwell", Font.BOLD, 18));
-		GridBagConstraints gbc_lblAerolnea = new GridBagConstraints();
-		gbc_lblAerolnea.insets = new Insets(0, 0, 5, 5);
-		gbc_lblAerolnea.gridx = 1;
-		gbc_lblAerolnea.gridy = 1;
-		contentPane.add(lblAerolnea, gbc_lblAerolnea);
 		
 		JLabel lblOrigen = new JLabel("Origen");
 		lblOrigen.setFont(new Font("Rockwell", Font.BOLD, 18));
 		GridBagConstraints gbc_lblOrigen = new GridBagConstraints();
 		gbc_lblOrigen.insets = new Insets(0, 0, 5, 5);
-		gbc_lblOrigen.gridx = 3;
+		gbc_lblOrigen.gridx = 1;
 		gbc_lblOrigen.gridy = 1;
 		contentPane.add(lblOrigen, gbc_lblOrigen);
 		
@@ -70,54 +64,51 @@ public class HacerReserva extends JFrame {
 		lblDestino.setFont(new Font("Rockwell", Font.BOLD, 18));
 		GridBagConstraints gbc_lblDestino = new GridBagConstraints();
 		gbc_lblDestino.insets = new Insets(0, 0, 5, 5);
-		gbc_lblDestino.gridx = 5;
+		gbc_lblDestino.gridx = 3;
 		gbc_lblDestino.gridy = 1;
 		contentPane.add(lblDestino, gbc_lblDestino);
-		
-		JComboBox comboBox = new JComboBox();
-		GridBagConstraints gbc_comboBox = new GridBagConstraints();
-		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
-		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
-		gbc_comboBox.gridx = 1;
-		gbc_comboBox.gridy = 3;
-		contentPane.add(comboBox, gbc_comboBox);
 		
 		JComboBox comboBox_1 = new JComboBox();
 		GridBagConstraints gbc_comboBox_1 = new GridBagConstraints();
 		gbc_comboBox_1.insets = new Insets(0, 0, 5, 5);
 		gbc_comboBox_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_comboBox_1.gridx = 3;
-		gbc_comboBox_1.gridy = 3;
+		gbc_comboBox_1.gridx = 1;
+		gbc_comboBox_1.gridy = 2;
 		contentPane.add(comboBox_1, gbc_comboBox_1);
 		
 		JComboBox comboBox_2 = new JComboBox();
 		GridBagConstraints gbc_comboBox_2 = new GridBagConstraints();
 		gbc_comboBox_2.insets = new Insets(0, 0, 5, 5);
 		gbc_comboBox_2.fill = GridBagConstraints.HORIZONTAL;
-		gbc_comboBox_2.gridx = 5;
-		gbc_comboBox_2.gridy = 3;
+		gbc_comboBox_2.gridx = 3;
+		gbc_comboBox_2.gridy = 2;
 		contentPane.add(comboBox_2, gbc_comboBox_2);
 		
-		JButton btnAtrs = new JButton("Atr\u00E1s");
-		GridBagConstraints gbc_btnAtrs = new GridBagConstraints();
-		gbc_btnAtrs.insets = new Insets(0, 0, 5, 5);
-		gbc_btnAtrs.gridx = 1;
-		gbc_btnAtrs.gridy = 7;
-		contentPane.add(btnAtrs, gbc_btnAtrs);
-		
-		JButton btnHistoriograma = new JButton("Historiograma");
-		GridBagConstraints gbc_btnHistoriograma = new GridBagConstraints();
-		gbc_btnHistoriograma.insets = new Insets(0, 0, 5, 5);
-		gbc_btnHistoriograma.gridx = 3;
-		gbc_btnHistoriograma.gridy = 7;
-		contentPane.add(btnHistoriograma, gbc_btnHistoriograma);
+		JList list = new JList();
+		GridBagConstraints gbc_list = new GridBagConstraints();
+		gbc_list.gridheight = 4;
+		gbc_list.gridwidth = 3;
+		gbc_list.insets = new Insets(0, 0, 5, 5);
+		gbc_list.fill = GridBagConstraints.BOTH;
+		gbc_list.gridx = 1;
+		gbc_list.gridy = 3;
+		contentPane.add(list, gbc_list);
 		
 		JButton btnHacerReserva = new JButton("Hacer Reserva");
 		GridBagConstraints gbc_btnHacerReserva = new GridBagConstraints();
 		gbc_btnHacerReserva.insets = new Insets(0, 0, 5, 5);
-		gbc_btnHacerReserva.gridx = 5;
+		gbc_btnHacerReserva.gridx = 2;
 		gbc_btnHacerReserva.gridy = 7;
 		contentPane.add(btnHacerReserva, gbc_btnHacerReserva);
+		
+		JButton button = new JButton("New button");
+		button.setHorizontalAlignment(SwingConstants.LEFT);
+		GridBagConstraints gbc_button = new GridBagConstraints();
+		gbc_button.anchor = GridBagConstraints.WEST;
+		gbc_button.insets = new Insets(0, 0, 0, 5);
+		gbc_button.gridx = 1;
+		gbc_button.gridy = 8;
+		contentPane.add(button, gbc_button);
 	}
 
 }
